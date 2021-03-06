275e0e1f2ed707eb304f4af2f3162eeba6d3de0a
Brad Hamill <brad@hamill.com>
2021-03-05 18:50:49 -0700

    initial commit

---
+++ api-gateway/src/server/middleware/injectSession.ts
@@ -0,0 +1,20 @@
+import { Request, Response, NextFunction } from 'express'
+
+import UsersService from '#root/adapters/UsersService'
+
+const injectSession = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  if (req.cookies.userSessionId) {
+    const userSession = await UsersService.fetchUserSession({
+      sessionId: req.cookies.userSessionId,
+    })
+    res.locals.userSession = userSession
+  }
+
+  return next()
+}
+
+export default injectSession
